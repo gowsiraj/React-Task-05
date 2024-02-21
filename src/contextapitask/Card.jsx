ece74a55c896dc5c6d4fa49709e0dbab4f42e3ed
gowsiraj <gowsalyasanjai@gmail.com>
2024-02-21 14:02:27 +0400

    changes in align

--- src/contextapitask/Card.jsx
+++ src/contextapitask/Card.jsx
@@ -44,21 +44,21 @@ function Card() {
         </div>
       </div>
       <div  className='ml-2 mr-2'>
+      <div className="container text-center mb-5">
+      <div className="row">  
       {
         data.map((item, index) => {
           return (
-            
-            <div key={index} className="container text-center mt-5 mb-5 relative">
-              <div className="row">
-                <div className="col-sm-6 col-md-6 col-lg-6 col-xl-6 cardstyle">
+           
+                <div  key={index} className="col-sm-12 col-md-12 col-lg-12 col-xl-12 cardstyle  mt-5 ">
                   <div className="mb-3">
                     <div className="row g-0">
                       <div className="col-md-4 mt-3 mb-2 ">
-                        <img src={item.images[0]} className="img-fluid rounded-start " alt="..." />
-                        <button type="button" class="btn btn-danger mt-5 absolute" onClick={()=>handleRemove(item.id)}>Remove</button>
+                        <img src={item.images[0]} className="img-fluid rounded " alt="..." />
+                       
                       </div>
-                      <div className="col-md-8 car-3">
-                        <div className="card-body mt-3">
+                      <div className="col-md-4 car-3">
+                        <div className="card-body mt-5">
                           <h5 className="card-title">{item.title}</h5>
                           <p className="card-text mt-2">
                             {item.description}
@@ -69,43 +69,50 @@ function Card() {
                           <p className="card-text mt-2">
                             Price : $ {item.price}
                           </p>
+                          </div>
+                          </div>
+
+                          <div className="col-md-4">
                           <div class="container text-center mt-5">
                             <div class="row">
-                              <div class="col-2 col-sm-2 col-lg-2 col-md-2 col-xl-2">
+                              <div class="col-2 col-sm-2 col-lg-2 col-md-2 col-xl-2 mt-5">
 
                                 <button onClick={() => handleIncrease(item.id, item.quantity || 1)}>+</button>
 
                               </div>
-                              <div class="col-2 col-sm-2 col-lg-2 col-md-2 col-xl-2">
+                              <div class="col-2 col-sm-2 col-lg-2 col-md-2 col-xl-2 mt-5">
 
                                 <p>{item.quantity ||1}</p>
 
                               </div>
 
-                              <div class="col-2 col-sm-2 col-lg-2 col-md-2 col-xl-2">
+                              <div class="col-2 col-sm-2 col-lg-2 col-md-2 col-xl-2 mt-5">
                                 <button onClick={()=>handleDecrease(item.id, item.quantity || 1)}>-</button>
                               </div>
-                              <div class="col-6 col-sm-6 col-lg-6 col-md-6 col-xl-6">
+                              <div class="col-6 col-sm-6 col-lg-6 col-md-6 col-xl-6 mt-5">
                                 <p className="card-text">
                                   total :${item.price*(item.quantity || 1)}
                                 </p>
+                                <button type="button" class="btn btn-danger mt-5" onClick={()=>handleRemove(item.id)}>Remove</button>
 
                               </div>
                             </div>
                           </div>
                         </div>
-                      </div>
+                      
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            
+               
           )
 
         })
       }
       </div>
+      </div>
+      </div>
+      
+    
 
 
     </>
